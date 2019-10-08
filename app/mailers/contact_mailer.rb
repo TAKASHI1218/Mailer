667c50748fcc20e29c6174458015e46afbe69531
TAKASHI1218 <t61342006@yahoo.co.jp>
2019-10-08 02:36:07 +0000

    Mailer cannot send

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: "t61342006@yahoo.co.jp", subject: "確認メール"
-  end 
+    mail to: "jackas.jackas.jackas@gmail.com", subject: "確認メール"
+  end
 end
